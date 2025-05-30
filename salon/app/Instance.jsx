62f57602e93b27c73ad09dbec13a076154b3cfa6
Sayanthssk <130476452+Sayanthssk@users.noreply.github.com>
2025-05-30 11:44:13 +0530

    Update Instance.jsx

--- salon/app/Instance.jsx
+++ salon/app/Instance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: 'http://192.168.1.12:8000',
+    baseURL: 'https://saloon-management-server.onrender.com',
     
   });
   export default instance
