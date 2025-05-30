be294a1844d0d95b13ad81369134ef6f49a4de98
Sayanthssk <sayanthskumar01@gmail.com>
2025-05-30 16:06:53 +0530

    11th commit

--- salon/render-build.sh
+++ salon/render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo "Installing dependencies..."
-npm install 
+npm install --legacy-peer-deps
 echo "Building Expo web app..."
 npx expo export:web
 echo "Build complete."
